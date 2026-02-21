fcfc61ee6419d095e18cc913a8f6fa67e3f7003d
adcodess <ad08singh@gmail.com>
2026-02-21 04:58:33 +0000

    basic operation,prefix,postfix

--- 01_basics/03_conversionOp.js
+++ 01_basics/03_conversionOp.js
@@ -7,12 +7,38 @@ let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
-/* 
-"33" => 33
-"33abc" => NaN
-true => 1; false => 0
-"" - empty string => 0 
-*/
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+//"" - empty string => 0 
+
+
 //To convert type, we can use String(number) or Number()
 //Note: Pay attention that first letter while type conversion is capital.
+console.log(2+2) // It runs.No need of ;
 
+// ***************basic op****************
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);   exponents
+//console.log(2/3);    quotient
+//console.log(2*3);    remainder
+
+let str1= "hello"
+let str2 = "adarsh"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);     //12
+console.log(1+"2");     //12
+console.log("1"+2+2);   //122
+console.log(2+2+"1");   //41
+
+let gamecounter=100;
+++gamecounter;
+console.log(gamecounter);
+
+//Read prefix postfix on mdn
